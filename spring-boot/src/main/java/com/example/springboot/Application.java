8d5dad7a7de58596c1db2f742db32789314af8d4
Kravatka <yegorkrava@gmail.com>
2023-07-21 23:49:51 +0300

    add index page and greating functional

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -2,16 +2,20 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		var app = SpringApplication.run(Application.class, args);
-		System.out.println(app.getBean(Application.class).hello());
 	}
 
-	public String hello() {
-		return "Hello World!";
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 }
