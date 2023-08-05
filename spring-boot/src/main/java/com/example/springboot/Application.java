d42def2722d9df2ad7371f44ae37bed4ac6abca5
Kravatka <yegorkrava@gmail.com>
2023-08-05 21:20:38 +0300

    fix linter

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@SuppressWarnings("PMD")
 public class Application {
 
 	public static void main(String[] args) {
