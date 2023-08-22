2e298bc7a4613e505e475e06aae35e273ddf5e49
bazzzket0810 <bazzzket@gmail.com>
2023-08-22 13:50:50 +0300

    fixed case of keywords in asdas.sql

--- members/bazzzket0810/spring-boot/DB/asdas.sql
+++ members/bazzzket0810/spring-boot/DB/asdas.sql
@@ -1,17 +1,19 @@
-create DATABASE IF NOT EXISTS `circumstence`;
+CREATE DATABASE IF NOT EXISTS `circumstence`;
 USE `circumstence`;
 
-drop table IF EXISTS `pie`;
+DROP TABLE IF EXISTS `pie`;
 
-create TABLE `pie`
+CREATE TABLE `pie`
 (
     `user` varchar(255) NOT NULL,
     `my_percent` int NOT NULL,
     `left_percent` int NOT NULL,
-    UNIQUE KEY `user_UNIQUE` (`user`)
+    UNIQUE KEY `user_unique` (`user`)
 ) ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
 SET character_set_client = @saved_cs_client;
 
-insert into `pie` VALUES ('basket0810', 6, 94);
+INSERT INTO `pie` VALUES ('basket0810', 6, 94);
+
